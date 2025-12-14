5e7606a5341fcb87145d387a0a9e6e0647d4253e
Dave <davidecannistraro02@gmail.com>
2025-12-14 11:38:41 +0100

    GitHub Pages e fix video

--- src/Components/Presentazione.jsx
+++ src/Components/Presentazione.jsx
@@ -2,40 +2,40 @@ import './Presentazione.css';
 import { useEffect, useRef } from 'react';
 
 export const Presentazione = () => {
-    const videoRef = useRef(null);
+  const videoRef = useRef(null);
 
-    useEffect(() => {
-        const video = videoRef.current;
-        if (!video) return;
+  useEffect(() => {
+    const video = videoRef.current;
+    if (!video) return;
 
-        const observer = new IntersectionObserver(
-            (entries) => {
-                entries.forEach((entry) => {
-                    if (entry.isIntersecting && entry.intersectionRatio >= 0.9) {
-                        video.play().catch(() => {
-                            // Se play viene interrotto, va in errore
-                        });
-                    } else {
-                        video.pause();
-                    }
-                });
-            },
-            { threshold: 0.9 }
-        );
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && entry.intersectionRatio >= 0.9) {
+            video.play().catch(() => {
+              // Se play viene interrotto, va in errore
+            });
+          } else {
+            video.pause();
+          }
+        });
+      },
+      { threshold: 0.9 }
+    );
 
-        observer.observe(video);
+    observer.observe(video);
 
-        return () => {
-            observer.disconnect();
-        };
-    }, []);
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
-    return (
-        <div className="presentazione-container" id="presentazione">
-            <div className="presentazione-card">
-                <h2>Soluzioni al tuo servizio</h2>
-                <video ref={videoRef} src="/Presentazione.mp4" muted controls></video>
-            </div>
-        </div>
-    )
+  return (
+    <div className="presentazione-container" id="presentazione">
+      <div className="presentazione-card">
+        <h2>Soluzioni al tuo servizio</h2>
+        <video ref={videoRef} src={`${import.meta.env.BASE_URL}Presentazione.mp4`} muted controls></video>
+      </div>
+    </div>
+  )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GiDi_React_Site/',
   server: {
-    host: '0.0.0.0',
+    host: '0.0.0.0',  // utile per accedere da altri dispositivi sulla rete locale
     port: 5173
   }
 })
+
